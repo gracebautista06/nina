f7036b426e3bb15f94cb12c5f9f4e9b59add5852
gracebautista06 <grcbtst06@gmail.com>
2026-04-23 09:14:40 +0800

    nag gama ko kay walalang

---
+++ header.php
@@ -0,0 +1,9 @@
+<header class="header">
+    <div class="logo">ICAS Property System</div>
+    <nav>
+        <a href="#">Dashboard</a>
+        <a href="#">Properties</a>
+        <a href="#">Reports</a>
+        <a href="#">Logout</a>
+    </nav>
+</header>
